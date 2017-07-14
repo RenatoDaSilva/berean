ee1a52ce7924271b5da98b507104d45ac9c2fdde
Renato da Silva <renato.katia@gmail.com>
2017-07-14 00:48:39 -0300

    Remove o termo sample_app do projeto

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,24 +5,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Berean"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Ajuda | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ajuda | Berean"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "Sobre | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Sobre | Berean"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contato | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contato | Berean"
   end
 end
