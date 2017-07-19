ea3783c6e4b9a8d65814826c958dfff028413f0e
Renato da Silva <renato.katia@gmail.com>
2017-07-19 00:00:38 -0300

    Ajustes de layout diversos

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,24 +5,24 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Berean - Gestor de igrejas"
   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Ajuda | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ajuda | Berean - Gestor de igrejas"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "Sobre | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Sobre | Berean - Gestor de igrejas"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contato | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contato | Berean - Gestor de igrejas"
   end
 end
